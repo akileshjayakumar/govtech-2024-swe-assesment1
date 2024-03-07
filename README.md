f665363f38d13590f3d8732d6d1a1beddef36dc7
Akilesh <ja694@uowmail.edu.au>
2024-03-07 23:15:39 +0800

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Getting Started with Create React App
+# Akilesh Jayakumar Assessment
+# gds-swe-gcc-assessment1-app
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [authorName, setAuthorName] = useState("");
+  const [submittedNames, setSubmittedNames] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSubmittedNames([...submittedNames, authorName]);
+    setAuthorName("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label className="labelcolor">
+          Enter An Author Name:
+          <input
+            type="text"
+            value={authorName}
+            onChange={(e) => setAuthorName(e.target.value)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+
+      <h1 className="h2color">Submitted Author Names</h1>
+      <ul>
+        {submittedNames.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+
+      <h1 className="h2color2">
+        List Of Author Names (Users) From Dummy API Website
+      </h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
